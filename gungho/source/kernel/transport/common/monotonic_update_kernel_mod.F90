036cdcdaa187d07a03030fcd3ac679653d538b6c
richardmalone <enquiries@metoffice.gov.uk>
2022-08-30 11:57:05 +0000

    ticket:3369 for Thomas Melvin: r36989 Extend simple monotonicity
    git-svn-id: https://metomi/svn/lfric.xm/LFRic/trunk@37774 4bbb03d4-c1cc-11e1-9ca4-71d347b01581

--- gungho/source/kernel/transport/common/monotonic_update_kernel_mod.F90
+++ gungho/source/kernel/transport/common/monotonic_update_kernel_mod.F90
@@ -7,21 +7,22 @@
 !-------------------------------------------------------------------------------
 
 !> @brief Kernel which enforces monotonicity on an advective update.
-!> @details Modifies the advective update A(theta) such that
-!!          \f[ M_\theta (theta^{n+1} - theta^{n}) + \Delta t A(\theta) \f]
-!!          Returns a monotonic theta^{n+1}. The monotonticity is enforced by
-!!          ensuring that the the implied theta^{n+1} lies within the range
-!!          [min(theta_s),max(theta_s)] for all s in the stencil of values used
-!!          to compute theta.
+!> @details Modifies the advective update A(field) such that
+!!          \f[ M_\field (field^{n+1} - field^{n}) + \Delta t A(\field) \f]
+!!          Returns a monotonic field^{n+1}. The monotonticity is enforced by
+!!          ensuring that the the implied field^{n+1} lies within the range
+!!          [min(field_s),max(field_s)] for all s in the stencil of values used
+!!          to compute field.
 module monotonic_update_kernel_mod
 
-use argument_mod,      only : arg_type,              &
-                              GH_FIELD, GH_SCALAR,   &
-                              GH_REAL, GH_INTEGER,   &
-                              GH_READWRITE, GH_READ, &
+use argument_mod,      only : arg_type,                  &
+                              GH_FIELD, GH_SCALAR,       &
+                              GH_REAL, GH_INTEGER,       &
+                              GH_LOGICAL,                &
+                              GH_READWRITE, GH_READ,     &
+                              ANY_DISCONTINUOUS_SPACE_1, &
                               STENCIL, CROSS, CELL_COLUMN
-use constants_mod,     only : r_def, i_def
-use fs_continuity_mod, only : Wtheta
+use constants_mod,     only : r_def, i_def, l_def
 use kernel_mod,        only : kernel_type
 
 implicit none
@@ -34,12 +35,12 @@ private
 !> The type declaration for the kernel. Contains the metadata needed by the PSy layer
 type, public, extends(kernel_type) :: monotonic_update_kernel_type
   private
-  type(arg_type) :: meta_args(5) = (/                                         &
-       arg_type(GH_FIELD,  GH_REAL,    GH_READWRITE, Wtheta),                 &
-       arg_type(GH_FIELD,  GH_REAL,    GH_READ,      Wtheta, STENCIL(CROSS)), &
-       arg_type(GH_FIELD,  GH_REAL,    GH_READ,      Wtheta),                 &
-       arg_type(GH_SCALAR, GH_REAL,    GH_READ),                              &
-       arg_type(GH_SCALAR, GH_INTEGER, GH_READ)                               &
+  type(arg_type) :: meta_args(5) = (/                                                            &
+       arg_type(GH_FIELD,  GH_REAL,    GH_READWRITE, ANY_DISCONTINUOUS_SPACE_1),                 &
+       arg_type(GH_FIELD,  GH_REAL,    GH_READ,      ANY_DISCONTINUOUS_SPACE_1, STENCIL(CROSS)), &
+       arg_type(GH_SCALAR, GH_REAL,    GH_READ),                                                 &
+       arg_type(GH_SCALAR, GH_LOGICAL, GH_READ),                                                 &
+       arg_type(GH_SCALAR, GH_LOGICAL, GH_READ)                                                  &
        /)
   integer :: operates_on = CELL_COLUMN
 contains
@@ -53,93 +54,84 @@ public :: monotonic_update_code
 
 contains
 
-!> @brief Computes the horizontal fluxes for a tracer density.
-!> @param[in]     nlayers      Number of layers
-!> @param[in,out] adv          Advective update field to apply monotonicity to
-!> @param[in]     theta        Field to be advected, used to compute the advective output
-!> @param[in]     stencil_size Size of the stencil (number of cells)
-!> @param[in]     stencil_map  Dofmaps for the stencil
-!> @param[in]     inv_mt       Lumped inverse of the Wtheta mass matrix
-!> @param[in]     dt           Timestep
-!> @param[in]     order        Desired polynomial order used to reconstruct the high order
-!!                             theta approximation
-!> @param[in]     ndf_wt       Number of degrees of freedom per cell
-!> @param[in]     undf_wt      Number of unique degrees of freedom for the tracer field
-!> @param[in]     map_wt       Dofmap for the tracer field
+!> @brief Apply a montonic update to a field by clipping it
+!> @param[in]     nlayers       Number of layers
+!> @param[in,out] adv           Advective update field to apply monotonicity to
+!> @param[in]     field         Field to be advected, used to compute the advective output
+!> @param[in]     stencil_size  Size of the stencil (number of cells)
+!> @param[in]     stencil_map   Dofmaps for the stencil
+!> @param[in]     dt            Timestep
+!> @param[in]     do_horizontal Do horizontal monotone update
+!> @param[in]     do_vertical   Do vertical monotone update
+!> @param[in]     ndf           Number of degrees of freedom per cell
+!> @param[in]     undf          Number of unique degrees of freedom for the tracer field
+!> @param[in]     map           Dofmap for the tracer field
 subroutine monotonic_update_code( nlayers,              &
                                   adv,                  &
-                                  theta,                &
+                                  field,                &
                                   stencil_size,         &
                                   stencil_map,          &
-                                  inv_mt,               &
                                   dt,                   &
-                                  global_order,         &
-                                  ndf_wt,               &
-                                  undf_wt,              &
-                                  map_wt)
+                                  do_horizontal,        &
+                                  do_vertical,          &
+                                  ndf,                  &
+                                  undf,                 &
+                                  map )
 
   implicit none
 
   ! Arguments
-  integer(kind=i_def),                                 intent(in) :: nlayers
-  integer(kind=i_def),                                 intent(in) :: ndf_wt
-  integer(kind=i_def),                                 intent(in) :: undf_wt
-  integer(kind=i_def),                                 intent(in) :: stencil_size
-  integer(kind=i_def), dimension(ndf_wt,stencil_size), intent(in) :: stencil_map
-  integer(kind=i_def), dimension(ndf_wt),              intent(in) :: map_wt
+  integer(kind=i_def),                              intent(in) :: nlayers
+  integer(kind=i_def),                              intent(in) :: ndf
+  integer(kind=i_def),                              intent(in) :: undf
+  integer(kind=i_def),                              intent(in) :: stencil_size
+  integer(kind=i_def), dimension(ndf,stencil_size), intent(in) :: stencil_map
+  integer(kind=i_def), dimension(ndf),              intent(in) :: map
 
-  real(kind=r_def), dimension(undf_wt), intent(inout) :: adv
-  real(kind=r_def), dimension(undf_wt), intent(in)    :: theta
-  real(kind=r_def), dimension(undf_wt), intent(in)    :: inv_mt
-  real(kind=r_def),                     intent(in)    :: dt
-  integer(kind=i_def),                  intent(in)    :: global_order
+  logical(kind=l_def), intent(in) :: do_horizontal, do_vertical
+
+  real(kind=r_def), dimension(undf), intent(inout) :: adv
+  real(kind=r_def), dimension(undf), intent(in)    :: field
+  real(kind=r_def),                  intent(in)    :: dt
 
   ! Internal variables
-  integer(kind=i_def) :: k, m, stencil, cell, p, ijkp, order, pmin, pmax
-  integer(kind=i_def), allocatable, dimension(:,:) :: smap
+  integer(kind=i_def) :: k, cell, p, ijkp, pmin, pmax, nl
 
-  real(kind=r_def) :: theta0, theta_max, theta_min, a_min, a_max
+  real(kind=r_def) :: field0, field_max, field_min, a_min, a_max
 
-  ! Compute the offset map for all even orders up to order
-  allocate( smap(global_order+3,0:global_order/2) )
-  smap(:,:) = 0
-  do m = 0,global_order,2
-    do stencil = 1,m+3
-      smap(stencil,m/2) = - 1 - m/2 + (stencil-1)
-    end do
-  end do
+  ! Upper limit
+  ! nl  == nlayers - 1 for W3 spaces
+  ! nl  == nlayers for Wtheta spaces
+  nl = (nlayers - 1) + (ndf - 1)
 
   ! Enforce monotonicity of the advective update across the stencil
   do k = 0, nlayers
-    theta0    = theta(stencil_map(1,1)+k)
-    theta_max = theta(stencil_map(1,1)+k)
-    theta_min = theta(stencil_map(1,1)+k)
+    field0    = field(stencil_map(1,1)+k)
+    field_max = field(stencil_map(1,1)+k)
+    field_min = field(stencil_map(1,1)+k)
     ! Max min across the horzontal stencil (this assumes a cross stencil which
     ! is incorrect for 2D interpolation but should still be a good guess)
-    do cell = 2,stencil_size
-      theta_max = max(theta_max,theta(stencil_map(1,cell)+k))
-      theta_min = min(theta_min,theta(stencil_map(1,cell)+k))
-    end do
-    ! Max min across the vertical stencil
-    order = min(global_order, min(2*(k-1), 2*(nlayers-1-k)))
-
-    ! These limits should be p=1,order+3k
-    ! and ijkp = stencil_map(1,1) + k + smap(p,order/2)
-    ! but this caused a segmentation fault so for now use simpler
-    ! k-1,k+1 limits
-    pmin = max(0,       k-1)
-    pmax = min(nlayers, k+1)
-    do p = pmin,pmax
-      ijkp = stencil_map(1,1) + p
-      theta_max = max(theta_max,theta(ijkp))
-      theta_min = min(theta_min,theta(ijkp))
-    end do
-    a_min = (theta0 - theta_max)/(dt*inv_mt(stencil_map(1,1)+k))
-    a_max = (theta0 - theta_min)/(dt*inv_mt(stencil_map(1,1)+k))
-    adv(map_wt(1)+k) = min(a_max,max(adv(stencil_map(1,1)+k),a_min))
+    if ( do_horizontal ) then
+      do cell = 2,stencil_size
+        field_max = max(field_max,field(stencil_map(1,cell)+k))
+        field_min = min(field_min,field(stencil_map(1,cell)+k))
+      end do
+    end if
+    if ( do_vertical ) then
+      ! Max min across the vertical stencil
+      ! Use simple k-1,k+1 limits
+      pmin = max(0,  k-1)
+      pmax = min(nl, k+1)
+      do p = pmin,pmax
+        ijkp = stencil_map(1,1) + p
+        field_max = max(field_max,field(ijkp))
+        field_min = min(field_min,field(ijkp))
+      end do
+    end if
+    a_min = (field0 - field_max)/dt
+    a_max = (field0 - field_min)/dt
+    adv(map(1)+k) = min(a_max,max(adv(stencil_map(1,1)+k),a_min))
   end do
-
-  deallocate( smap )
 
 end subroutine monotonic_update_code
 
