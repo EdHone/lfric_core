62078ea9a5728c19ed5d7f365f6d320e6e90ab1c
jenniferhickson <enquiries@metoffice.gov.uk>
2023-03-09 10:57:13 +0000

    ticket:3696 for Maggie Hendry : Update LFRic coupled miniapp Rivers app to be consistent with jules:ticket:1317
    git-svn-id: https://metomi/svn/lfric.xm/LFRic/trunk@41216 4bbb03d4-c1cc-11e1-9ca4-71d347b01581

--- lfric_atm/fcm-make/parameters.sh
+++ lfric_atm/fcm-make/parameters.sh
@@ -14,8 +14,14 @@
 # variables below are empty, and the revisions of any projects with
 # dependent changes should be updated to the revision at which those
 # changes were committed to the project's trunk
+
 export casim_rev=10024
 export casim_sources=
+#******************************************************************************
+# Please ensure that JULES_SOURCE_VN in the Rivers build
+# miniapps/lfric_coupled/rose-stem/app/fcm_make_river/rose-app.conf
+# is consistent with jules_rev below.
+#******************************************************************************
 export jules_rev=25146
 export jules_sources=
 export shumlib_rev=um13.1
