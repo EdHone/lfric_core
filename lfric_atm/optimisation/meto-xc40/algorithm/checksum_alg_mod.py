0e13c99e544ecc90802f2c775503881bb5d80faa
mikehobson <enquiries@metoffice.gov.uk>
2024-01-24 08:46:26 +0000

    ticket:3196 for Iva Kavcic: Improve build system portability for PSyclone.
    git-svn-id: https://metomi/svn/lfric.xm/LFRic/trunk@48256 4bbb03d4-c1cc-11e1-9ca4-71d347b01581

--- lfric_atm/optimisation/meto-xc40/algorithm/checksum_alg_mod.py
+++ lfric_atm/optimisation/meto-xc40/algorithm/checksum_alg_mod.py
@@ -4,9 +4,10 @@
 # under which the code may be used.
 ##############################################################################
 
-'''PSyclone transformation script for the Dynamo0p3 API to apply
-colouring and redundant computation to the level1 halo for
-setval_c generically.
+'''
+PSyclone transformation script for the LFRic (Dynamo0p3) API to apply
+colouring and redundant computation to the level-1 halo for
+the initialisation built-ins generically.
 
 The inner product used by the checksum algorithm produces different
 results on different numbers of threads if OpenMP is applied. It means
@@ -15,50 +16,15 @@ same. Therefore, we do not apply the OpenMP transform to the
 algorithm.
 
 '''
-from psyclone.transformations import Dynamo0p3ColourTrans, \
-                                     Dynamo0p3RedundantComputationTrans
-
-from psyclone.domain.lfric import LFRicConstants
+from psyclone_tools import (redundant_computation_setval, colour_loops,
+                            view_transformed_schedule)
 
 
 def trans(psy):
-    '''Applies PSyclone colouring and redundant computation
-    transformations.
+    '''
+    Applies PSyclone colouring and redundant computation transformations.
 
     '''
-    ctrans = Dynamo0p3ColourTrans()
-    rtrans = Dynamo0p3RedundantComputationTrans()
-    const = LFRicConstants()
-
-    setval_count = 0
-    # Loop over all of the Invokes in the PSy object
-    for invoke in psy.invokes.invoke_list:
-
-        print(f"Transforming invoke '{invoke.name}' ...")
-        schedule = invoke.schedule
-
-        # Make setval_c compute redundantly to the level 1 halo if it
-        # is in its own loop.
-        for loop in schedule.loops():
-            if loop.iteration_space == "dof":
-                if len(loop.kernels()) != 1:
-                    raise Exception(
-                        f"Expecting loop to contain 1 call but found "
-                        f"'{len(loop.kernels())}'")
-                if loop.kernels()[0].name in ["setval_c"]:
-                    setval_count += 1
-                    rtrans.apply(loop, options={"depth": 1})
-
-        # Colour loops over cells unless they are on discontinuous
-        # spaces or over dofs
-        for loop in schedule.loops():
-            if loop.iteration_space == "cell_column" \
-                and loop.field_space.orig_name \
-                    not in const.VALID_DISCONTINUOUS_NAMES:
-                ctrans.apply(loop)
-
-        # Take a look at what we've done
-        print(f"Found {setval_count} setval calls")
-        print(schedule.view())
-
-    return psy
+    redundant_computation_setval(psy)
+    colour_loops(psy)
+    view_transformed_schedule(psy)
