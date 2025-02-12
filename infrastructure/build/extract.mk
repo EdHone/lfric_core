e3e1f0283656fc2b7f774d92f254cf9f43cb864a
andrewcoughtrie <enquiries@metoffice.gov.uk>
2025-02-12 10:37:16 +0000

    ticket:4510 for Jennifer Hickson : Build changes for Physics Fork
    git-svn-id: https://metomi/svn/lfric.xm/LFRic/trunk@52041 4bbb03d4-c1cc-11e1-9ca4-71d347b01581

--- infrastructure/build/extract.mk
+++ infrastructure/build/extract.mk
@@ -7,7 +7,7 @@
 # Run this make file to copy a source tree from SOURCE_DIR to WORKING_DIR
 #
 .PHONY: files-to-extract
-files-to-extract: $(addprefix $(WORKING_DIR)/,$(shell find $(SOURCE_DIR) -name '*.[Ff]90' -print | sed "s|$(SOURCE_DIR)/||")) \
+files-to-extract: $(addprefix $(WORKING_DIR)/,$(shell find $(SOURCE_DIR) \( -name '*.[Ff]90' -o -name '*.h' \) -print | sed "s|$(SOURCE_DIR)/||")) \
                   | $(WORKING_DIR)
 
 .PRECIOUS: $(WORKING_DIR)/%.F90
@@ -24,6 +24,12 @@ $(WORKING_DIR)/%.f90: $(SOURCE_DIR)/%.f90 | $(WORKING_DIR)
 
 .PRECIOUS: $(WORKING_DIR)/%.nld
 $(WORKING_DIR)/%.nld: $(SOURCE_DIR)/%.nld | $(WORKING_DIR)
+	$(call MESSAGE,Copying source,$<)
+	$(Q)mkdir -p $(dir $@)
+	$(Q)cp $< $@
+
+.PRECIOUS: $(WORKING_DIR)/%.h
+$(WORKING_DIR)/%.h: $(SOURCE_DIR)/%.h | $(WORKING_DIR)
 	$(call MESSAGE,Copying source,$<)
 	$(Q)mkdir -p $(dir $@)
 	$(Q)cp $< $@
