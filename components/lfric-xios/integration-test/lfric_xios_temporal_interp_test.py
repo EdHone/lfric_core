2424f544a0aa90733e10526da4c5126bb61fb15b
EdHone <edward.hone@metoffice.gov.uk>
2026-02-09 20:16:37 +0000

    Non sync test working and implementation fixed

--- components/lfric-xios/integration-test/lfric_xios_temporal_interp_test.py
+++ components/lfric-xios/integration-test/lfric_xios_temporal_interp_test.py
@@ -55,8 +55,48 @@ class LfricXiosFullInterpTest(LFRicXiosTest):  # pylint: disable=too-few-public-
 
         return "Reading and interpolating data okay..."
 
+class LfricXiosNonSyncInterpTest(LFRicXiosTest):  # pylint: disable=too-few-public-methods
+    """
+    Tests the LFRic-XIOS temporal reading functionality for a full set of non-cyclic data
+    """
+
+    def __init__(self):
+        super().__init__(command=[sys.argv[1], "resources/configs/cyclic_high_freq.nml"], processes=1)
+        test_data_dir = Path(Path.cwd(), 'resources/data')
+        Path('lfric_xios_interp_input.nc').unlink(missing_ok=True)
+        self.gen_data(Path(test_data_dir, 'temporal_data.cdl'), Path('lfric_xios_interp_input.nc'))
+        self.gen_data(Path(test_data_dir, 'cyclic_high_freq_kgo.cdl'), Path('cyclic_high_freq_kgo.nc'))
+        self.gen_config( Path("resources/configs/cyclic_base.nml"),
+                         Path("resources/configs/cyclic_high_freq.nml"),
+                         {"dt":"10.0",
+                          "calendar_start":"'2024-01-01 15:03:20'",
+                          "timestep_end":"'30'"} )
+
+    def test(self, returncode: int, out: str, err: str):
+        """
+        Test the output of the context test
+        """
+
+        if returncode != 0:
+            print(out)
+            raise TestFailed(f"Unexpected failure of test executable: {returncode}\n" +
+                             f"stderr:\n" +
+                             f"{err}")
+
+        self.plot_output(Path('lfric_xios_interp_input.nc'),
+                         Path('lfric_xios_interp_output.nc'),
+                         'temporal_field')
+
+        if not self.nc_data_match(Path('lfric_xios_interp_input.nc'),
+                                  Path('lfric_xios_interp_output.nc'),
+                                  'temporal_field'):
+            raise TestFailed("Output data does not match input data for same time values")
+
+        return "Reading and interpolating data okay..."
+
 
 
 ##############################################################################
 if __name__ == "__main__":
     TestEngine.run(LfricXiosFullInterpTest())
+    TestEngine.run(LfricXiosNonSyncInterpTest())
