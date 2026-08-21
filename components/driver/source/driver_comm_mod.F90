f6950a68983b0c89e62815d178391ce589c74885
EdHone <edward.hone@metoffice.gov.uk>
2026-08-21 12:17:42 +0100

    Final xios last

--- components/driver/source/driver_comm_mod.F90
+++ components/driver/source/driver_comm_mod.F90
@@ -140,18 +140,6 @@ contains
     type(coupling_type), pointer :: coupling_ptr
 #endif
 
-    write(output_unit, '(A)') 'MPI final'
-    flush(output_unit)
-    ! Finalise the mpi object
-    call modeldb%mpi%finalise()
-
-#ifdef USE_XIOS
-    write(output_unit, '(A)') 'XIOS final'
-    flush(output_unit)
-    ! Finalise XIOS
-    call lfric_xios_finalise()
-#endif
-
 #ifdef MCT
     write(output_unit, '(A)') 'Oasis final'
     flush(output_unit)
@@ -166,9 +154,20 @@ contains
     ! Finalise halo exchange functionality
     call finalise_halo_comms()
 
+    write(output_unit, '(A)') 'MPI final'
+    flush(output_unit)
+    ! Finalise the mpi object
+    call modeldb%mpi%finalise()
     ! Release the communicator if it is ours to release. If a communicator has
     ! been provided to LFRic, then that is someone else's responsibility
     if(comm_created)call destroy_comm()
+
+#ifdef USE_XIOS
+    write(output_unit, '(A)') 'XIOS final'
+    flush(output_unit)
+    ! Finalise XIOS
+    call lfric_xios_finalise()
+#endif
 
     write(output_unit, '(A)') 'complete final'
     flush(output_unit)
